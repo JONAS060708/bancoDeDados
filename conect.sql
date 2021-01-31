7c30164052487a61a66dd41af17bc0c643d6cf3c
Jonas Cabral <cabral_jonascabral@hotmail.com>
2021-01-31 18:09:34 -0300

    primeiros comandos CREATE

---
+++ conect.sql
@@ -0,0 +1,15 @@
+/*          PRIMEIROS COMANDOS
+
+CREATE DATABASE cadastrosvs;*/
+
+/*use cadastrovs;
+create table pessoasvs(
+nome varchar(30),
+idade tinyint,
+sexo char(1),
+altura float,
+peso float,
+nacionalidade varchar(20)
+);*/
+
+DESCRIBE pessoasvs;
---
+++ teoria.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ *                      **mysql**
+ * DDL -> DEFINIÇÃO
+ * DML -> MANIPULAÇÃO
+ * DQL -> SOLICITAÇÕES
+ * DCL -> CONTROLE 
+ * DTL -> TRANSAÇÕES
+ * 
+ * D -> durabilidade
+ * I -> isolamento
+ * C -> consistência
+ * A -> atomicidade
+ * 
+ *              INSTALANDO XAMPP / Workbench
+ * 
+ *                      TIPOS PRIMITIVOS
+ *MUMÉRICO
+         ->INTEIRO->TinyInt, SmallInt, Int, MediumInt, BigInt
+         ->REAL -> Decimal, Float, Double, Real
+         ->LÓGICO -> Bit, Boolean
+ *DATA / TEMPO 
+         -> Date, DateTime, TimeStamp, Time, Year
+ *LITERAL 
+        -> CARACTER -> Char,Varchar
+        -> TEXTO -> TinyText, Text, MediumText, LongText
+        ->BINÁRIO -> TinyBlob, Blob, MediumBlob, LongBlob
+        ->COLEÇÃO -> Enum, Set
+ *ESPACIAL 
+        -> Geometry, Point, Polygon, MultiPolygon
+ * 
+ */

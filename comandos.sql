f888da1a2fbae7de56b4a07e45b3757485786512
Jonas Cabral <cabral_jonascabral@hotmail.com>
2021-02-04 09:13:28 -0300

    COMANDOS MYSQL

--- comandos.sql
+++ comandos.sql
@@ -30,6 +30,35 @@ create table pessoas(
     primary key (id) 							/*DEFINE COMO CHAVE PRIMARIA O CAMPO ESCOLHIDO*/
 )DEFAULT charset = utf8;
 
+/*CRIANDO NOVA TABELA*/
+create table if not exists cursos(
+nome varchar(30) not null unique,/*NÃO DEIXA COLOCAR O MESMO NOME NO CAMPO*/
+descricao text,
+carga int unsigned,/*SEM SINAL NÃO DEIXA COLAR SINAL NO NÚMERO */
+totaulas int,
+ano year default '2021'
+) default charset 'utf8';
+
+/*ADICIONANDO DADOS NA TABELA*/
+insert into cursos(nome,descricao,carga,totaulas,ano) value
+('PROGRAMAÇÃO','basico de programação com jonas','40','15','2021'),
+('PHP','basico de  programação PHP com jonas','40','32','2022');
+
+/*MOSTRANDO DADOS DA TABELA*/
+select * FROM cursos;
+
+/*COLOCANDO COLUNA NA PRIMEIRA POSIÇÃO */
+ALTER TABLE cursos ADD COLUMN idcursos INT FIRST;
+
+/* ALTERANDO TIPO DE CONSTRAINS DA  COLUNA */
+alter table cursos
+modify column idcursos int(3) auto_increment;/*MODIFICA A COLUNA*/
+alter TABLE	cursos ADD primary key(idcursos);
+describe cursos;
+
+/*APAGANDO TABLELA*/
+DROP TABLE cursos;
+
 /*INSERINDO DADOS NA TABELA*/
 /*[ DML ] data manipulation langrage --  comandos de manipulação*/
 insert into 
@@ -40,4 +69,22 @@ values
 ('NICOLLE','2016-03-10','F','0.75','35.5',default);
 
 SELECT * FROM cadastro.pessoas; /*SELECIONA TODOS OS DADOS DA TABELA*/
+
+alter table pessoas
+add column profissao varchar(10) after nome; /*adicionando campos na tabela, apos a column desejada*/
+
+
+alter table pessoas
+drop column profissao;
+
+alter table pessoas
+modify column profissao varchar(20) not null default '';/*MODIFICA A COLUNA*/
+describe pessoas;
+/*RENOMEANDO COLUNA*/
+alter table pessoas
+change column profissao prof varchar(20);
+/*RENOMEANDO TABELA*/
+alter table pessoas rename to gafanhotos;
+alter table gafanhotos rename to pessoas;
+
 DELETE FROM `cadastro`.`pessoas` WHERE (`id` = '1');

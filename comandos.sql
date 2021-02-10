6660b5dff4ba81f56093ce551f0f185613d0bb44
Jonas Cabral <cabral_jonascabral@hotmail.com>
2021-02-10 09:18:09 -0300

    Update comandos.sql

--- comandos.sql
+++ comandos.sql
@@ -1,4 +1,7 @@
-SELECT * FROM cadastro.cursos; /*SELECIONA TODOS OS DADOS DA TABELA*/
+SELECT * FROM gafanhotos where nome like '$%'; /*SELECIONA TODOS OS DADOS DA TABELA*/
+select * from gafanhotos;
+DELETE FROM `cadastro`.`gafanhotos` WHERE nome like 'jonas%';
 describe cursos; /*MOSTRA AS COLULAS DA TABELA*/
 
 
+insert into gafanhotos(nome) values('Jonas Pedroso Cabral');
